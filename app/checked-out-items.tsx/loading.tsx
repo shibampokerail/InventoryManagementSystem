b9d13293338c2dc5c4c37179786055cdc2f55961
rjaswal1634 <rjaswal1634@gmail.com>
2025-04-18 17:35:36 -0400

    fixed auth0

--- app/checked-out-items.tsx/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <div className="h-16 w-16 animate-spin rounded-full border-4 border-purple-200 border-t-purple-700"></div>
-      </div>
-    )
-  }
-  
